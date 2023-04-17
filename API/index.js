eb5f75172b5a7dfb041617ad57c51062fa02b9ca
Gustavo Minguta <47895351+mIINguta@users.noreply.github.com>
2023-04-17 14:04:01 -0300

    update index.js
    utilizando put para modificar informações

--- API/index.js
+++ API/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
+let author = "Gustavo Minguta"
+
 app.listen('3000')
 console.log('Rodando')
 
-app.route('/').get((req,res) => res.send("Hello"))
+//midleware (ponte entre as requisições. usado para entender o q esta sendo requisitado)
+app.use(express.json())
 
-
+app.route('/').get((req,res) => res.send(author))
+app.route('/').put((req,res) =>{
+    author = req.body.author
+    res.send(author)
+} )
