d25d991806b607dffaaa2919459c3f5594856c42
Gustavo Minguta <47895351+mIINguta@users.noreply.github.com>
2023-04-19 09:23:22 -0300

    update index.js
    Body Params

--- API/index.js
+++ API/index.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const app = express()
 
-let author = "Gustavo Minguta"
-
 app.listen('3000')
 console.log('Rodando')
 
 //midleware (ponte entre as requisições. usado para entender o q esta sendo requisitado)
 app.use(express.json())
 
-app.route('/').get((req,res) => res.send(author))
-app.route('/').put((req,res) =>{
-    author = req.body.author
-    res.send(author)
-} )
+app.route('/').post((req,res) => {
+     res.send(`Chegou aqui marujo: ${req.body.nome}`)
+     console.log(req.body)
+})
+
