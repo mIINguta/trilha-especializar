399e76ebc0641f635503da7dd273dc474dde9882
Gustavo Minguta <47895351+mIINguta@users.noreply.github.com>
2023-04-19 10:17:35 -0300

    update index.js
    Consumindo api do git hub com axios e recebendo no insomnia

--- API/index.js
+++ API/index.js
@@ -1,22 +1,17 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
 
 app.listen('3000')
-console.log('Rodando')
 
-//midleware (ponte entre as requisições. usado para entender o q esta sendo requisitado)
-app.use(express.json())
 
-app.route('/').get((req,res)=> {
-     res.send(req.query)
-     console.log(req.query)
+
+app.route('/').get((req,res) =>{
+
+axios.get("https://api.github.com/users/mIINguta/repos")
+.then(result => res.send(result.data))
+.catch(erro => console.error(erro))
+
 })
 
-app.route('/about/user').get((req,res)=> {
-     res.send(req.query)
-     console.log(req.query)
-})
 
-// a sintaxe da url é:
-//localhost:3000/?nome=Gustavo
-//localhost:3000/?nome=Gustavo&cidade=Rio de Janeiro
